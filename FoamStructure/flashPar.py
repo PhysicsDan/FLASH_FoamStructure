e318106bb16750ea9a9ea5555d7ecaddbac5df4e
Daniel Molloy <dmolloy@Daniels-MacBook-Air.local>
2026-04-15 10:26:14 +0200

    Modified to enable multiple layers and custom density structure. UNTESTED Some whitespace modifications too

--- FoamStructure/flashPar.py
+++ FoamStructure/flashPar.py
@@ -1,3 +1,123 @@
+import os
+
+#########################################
+#                                       #
+#     TARGET LAYER DEFINITIONS          #
+#                                       #
+#########################################
+# Define target layers as a list of dicts. Layers stack sequentially
+# in the y-direction: vacuum -> layer[0] -> layer[1] -> ...
+# The chamber (cham) species is always present as the background fill.
+#
+# Each layer dict MUST contain:
+#   name     : short species name (e.g. "tar1") - used in setup line
+#   rho      : initial density [g/cc]
+#   tele     : initial electron temperature [K]
+#   tion     : initial ion temperature [K]
+#   trad     : initial radiation temperature [K]
+#   A        : atomic mass
+#   Z        : atomic number
+#   zMin     : minimum zbar (set 0.0 if not needed)
+#   height   : layer thickness in y-direction [cm]
+#   radius   : layer radial extent [cm]
+#   eosType  : EOS type string ("eos_tab" or "eos_gam")
+#   eosSubType: EOS sub-type string (e.g. "ionmix4")
+#   eosFile  : EOS table filename
+#   opAbsorb : opacity absorption model (e.g. "op_tabpa")
+#   opEmiss  : opacity emission model (e.g. "op_tabpe")
+#   opTrans  : opacity transport model (e.g. "op_tabro")
+#   opFileType: opacity file type (e.g. "ionmix4")
+#   opFileName: opacity table filename
+
+LAYERS = [
+    {
+        "name": "targ",
+        "rho": 2.7,
+        "tele": 290.11375,
+        "tion": 290.11375,
+        "trad": 290.11375,
+        "A": 26.9815386,
+        "Z": 13.0,
+        "zMin": 0.02,
+        "height": 20.0e-04,
+        "radius": 200.0e-04,
+        "eosType": "eos_tab",
+        "eosSubType": "ionmix4",
+        "eosFile": "al-imx-003.cn4",
+        "opAbsorb": "op_tabpa",
+        "opEmiss": "op_tabpe",
+        "opTrans": "op_tabro",
+        "opFileType": "ionmix4",
+        "opFileName": "al-imx-003.cn4",
+    },
+    # Example: uncomment to add a second layer
+    # {
+    #     "name": "tar2",
+    #     "rho": 1.0,
+    #     "tele": 290.11375,
+    #     "tion": 290.11375,
+    #     "trad": 290.11375,
+    #     "A": 12.011,
+    #     "Z": 6.0,
+    #     "zMin": 0.0,
+    #     "height": 10.0e-04,
+    #     "radius": 200.0e-04,
+    #     "eosType": "eos_tab",
+    #     "eosSubType": "ionmix4",
+    #     "eosFile": "c-imx-001.cn4",
+    #     "opAbsorb": "op_tabpa",
+    #     "opEmiss": "op_tabpe",
+    #     "opTrans": "op_tabro",
+    #     "opFileType": "ionmix4",
+    #     "opFileName": "c-imx-001.cn4",
+    # },
+]
+
+#########################################
+#                                       #
+#     CHAMBER (BACKGROUND) DEFINITION   #
+#                                       #
+#########################################
+CHAMBER = {
+    "rho": 1.0e-06,
+    "tele": 290.11375,
+    "tion": 290.11375,
+    "trad": 290.11375,
+    "A": 4.002602,
+    "Z": 2.0,
+    "eosType": "eos_tab",
+    "eosSubType": "ionmix4",
+    "eosFile": "he-imx-005.cn4",
+    "opAbsorb": "op_tabpa",
+    "opEmiss": "op_tabpe",
+    "opTrans": "op_tabro",
+    "opFileType": "ionmix4",
+    "opFileName": "he-imx-005.cn4",
+}
+
+#########################################
+#                                       #
+#     CUSTOM DENSITY MAP OPTIONS        #
+#                                       #
+#########################################
+# If True, density and species at each cell are read from an external
+# .npz file instead of using the geometric layer definitions.
+# The .npz file must contain:
+#   x       : 1D array of x (radial) cell-centre coordinates [cm]
+#   y       : 1D array of y (axial) cell-centre coordinates [cm]
+#   density : 2D array shape (len(x), len(y)) of density values [g/cc]
+#   species : 2D array shape (len(x), len(y)) of integer species indices
+#             (0 = cham, 1 = first layer, 2 = second layer, ...)
+CUSTOM_DENS_MAP = False
+CUSTOM_DENS_MAP_FILE = "density_map.npz"
+
+
+def _species_string():
+    """Build the species= setup argument from LAYERS."""
+    names = ["cham"] + [layer["name"] for layer in LAYERS]
+    return "species=" + ",".join(names)
+
+
 def setupArgs():
     args = [
             '-auto',
@@ -6,7 +126,7 @@ def setupArgs():
             '-nxb=16',
             '-nyb=16',
             '+hdf5typeio',
-            'species=cham,targ',
+            _species_string(),
             '+mtmmmt',
             '+laser',
             '+uhd3t',
@@ -19,9 +139,9 @@ def setupArgs():
 
 def parms():
     import flash.parm as p
-    p.run_comment("Laser Slab Example Simulation") 
-    p.log_file("lasslab.log") 
-    p.basenm("lasslab_") 
+    p.run_comment("Laser Slab Example Simulation")
+    p.log_file("lasslab.log")
+    p.basenm("lasslab_")
 
 # This particular parfile is used as an example that is described in
 # detail in the users guide.
@@ -33,66 +153,73 @@ def parms():
 ##########################
 
 ### Checkpoint Options  ###
-    p.checkpointFileIntervalTime(1.0) 
-    p.checkpointFileIntervalStep(1000) 
+    p.checkpointFileIntervalTime(1.0)
+    p.checkpointFileIntervalStep(1000)
 
 ### Plot Options ###
-    p.plotFileNumber(0) 
-    p.plotFileIntervalStep(100) 
-    p.plotFileIntervalTime(0.01e-09) 
-    p.plot_var_1("dens") 
-    p.plot_var_2("depo") 
-    p.plot_var_3("tele") 
-    p.plot_var_4("tion") 
-    p.plot_var_5("trad") 
-    p.plot_var_6("ye  ") 
-    p.plot_var_7("sumy") 
-    p.plot_var_8("cham") 
-    p.plot_var_9("targ") 
+    p.plotFileNumber(0)
+    p.plotFileIntervalStep(100)
+    p.plotFileIntervalTime(0.01e-09)
+    p.plot_var_1("dens")
+    p.plot_var_2("depo")
+    p.plot_var_3("tele")
+    p.plot_var_4("tion")
+    p.plot_var_5("trad")
+    p.plot_var_6("ye  ")
+    p.plot_var_7("sumy")
+    p.plot_var_8("cham")
+
+    # Dynamically add plot vars for each layer species
+    plot_idx = 9
+    for layer in LAYERS:
+        getattr(p, f"plot_var_{plot_idx}")(layer["name"])
+        plot_idx += 1
 
 ### Restart Options ###
-    p.restart(False) 
-    p.checkpointFileNumber(0) 
+    p.restart(False)
+    p.checkpointFileNumber(0)
 
 ########################################
 #                                      #
 #     RADIATION/OPACITY PARAMETERS     #
 #                                      #
 ########################################
-    p.rt_useMGD(True) 
-    p.rt_mgdNumGroups(6) 
-    p.rt_mgdBounds_1(1.0e-01) 
-    p.rt_mgdBounds_2(1.0e+00) 
-    p.rt_mgdBounds_3(1.0e+01) 
-    p.rt_mgdBounds_4(1.0e+02) 
-    p.rt_mgdBounds_5(1.0e+03) 
-    p.rt_mgdBounds_6(1.0e+04) 
-    p.rt_mgdBounds_7(1.0e+05) 
-    p.rt_mgdFlMode("fl_harmonic") 
-    p.rt_mgdFlCoef(1.0) 
+    p.rt_useMGD(True)
+    p.rt_mgdNumGroups(6)
+    p.rt_mgdBounds_1(1.0e-01)
+    p.rt_mgdBounds_2(1.0e+00)
+    p.rt_mgdBounds_3(1.0e+01)
+    p.rt_mgdBounds_4(1.0e+02)
+    p.rt_mgdBounds_5(1.0e+03)
+    p.rt_mgdBounds_6(1.0e+04)
+    p.rt_mgdBounds_7(1.0e+05)
+    p.rt_mgdFlMode("fl_harmonic")
+    p.rt_mgdFlCoef(1.0)
 
-    p.rt_mgdXlBoundaryType("reflecting") 
-    p.rt_mgdXrBoundaryType("vacuum") 
-    p.rt_mgdYlBoundaryType("vacuum") 
-    p.rt_mgdYrBoundaryType("reflecting") 
-    p.rt_mgdZlBoundaryType("reflecting") 
-    p.rt_mgdZrBoundaryType("reflecting") 
+    p.rt_mgdXlBoundaryType("reflecting")
+    p.rt_mgdXrBoundaryType("vacuum")
+    p.rt_mgdYlBoundaryType("vacuum")
+    p.rt_mgdYrBoundaryType("reflecting")
+    p.rt_mgdZlBoundaryType("reflecting")
+    p.rt_mgdZrBoundaryType("reflecting")
 
-    p.useOpacity(True) 
+    p.useOpacity(True)
 
 ### SET CHAMBER (HELIUM) OPACITY OPTIONS ###
-    p.op_chamAbsorb("op_tabpa") 
-    p.op_chamEmiss("op_tabpe") 
-    p.op_chamTrans("op_tabro") 
-    p.op_chamFileType("ionmix4") 
-    p.op_chamFileName("he-imx-005.cn4") 
+    p.op_chamAbsorb(CHAMBER["opAbsorb"])
+    p.op_chamEmiss(CHAMBER["opEmiss"])
+    p.op_chamTrans(CHAMBER["opTrans"])
+    p.op_chamFileType(CHAMBER["opFileType"])
+    p.op_chamFileName(CHAMBER["opFileName"])
 
-### SET TARGET (ALUMINUM) OPACITY OPTIONS ###
-    p.op_targAbsorb("op_tabpa") 
-    p.op_targEmiss("op_tabpe") 
-    p.op_targTrans("op_tabro") 
-    p.op_targFileType("ionmix4") 
-    p.op_targFileName("al-imx-003.cn4") 
+### SET TARGET LAYER OPACITY OPTIONS ###
+    for layer in LAYERS:
+        name = layer["name"]
+        getattr(p, f"op_{name}Absorb")(layer["opAbsorb"])
+        getattr(p, f"op_{name}Emiss")(layer["opEmiss"])
+        getattr(p, f"op_{name}Trans")(layer["opTrans"])
+        getattr(p, f"op_{name}FileType")(layer["opFileType"])
+        getattr(p, f"op_{name}FileName")(layer["opFileName"])
 
 
 ############################
@@ -100,59 +227,59 @@ def parms():
 #     LASER PARAMETERS     #
 #                          #
 ############################
-    p.useEnergyDeposition(True) 
-    p.ed_maxRayCount(10000) 
-    p.ed_gradOrder(2) 
+    p.useEnergyDeposition(True)
+    p.ed_maxRayCount(10000)
+    p.ed_gradOrder(2)
 
 # Activate 3D-in-2D ray trace:
-    p.ed_laser3Din2D(True) 
-    p.ed_laser3Din2DwedgeAngle(0.1) 
+    p.ed_laser3Din2D(True)
+    p.ed_laser3Din2DwedgeAngle(0.1)
 
 
 ### LASER IO OPTIONS ###
-    p.ed_useLaserIO(True) 
-    p.ed_laserIOMaxNumberOfPositions(10000) 
-    p.ed_laserIOMaxNumberOfRays(128) 
+    p.ed_useLaserIO(True)
+    p.ed_laserIOMaxNumberOfPositions(10000)
+    p.ed_laserIOMaxNumberOfRays(128)
 
 ### SETUP LASER PULSES ###
-    p.ed_numberOfPulses(1) 
+    p.ed_numberOfPulses(1)
 
 # Define Pulse 1:
-    p.ed_numberOfSections_1(4) 
-    p.ed_time_1_1(0.0) 
-    p.ed_time_1_2(0.1e-09) 
-    p.ed_time_1_3(1.0e-09) 
-    p.ed_time_1_4(1.1e-09) 
+    p.ed_numberOfSections_1(4)
+    p.ed_time_1_1(0.0)
+    p.ed_time_1_2(0.1e-09)
+    p.ed_time_1_3(1.0e-09)
+    p.ed_time_1_4(1.1e-09)
 
-    p.ed_power_1_1(0.0) 
-    p.ed_power_1_2(1.0e+09) 
-    p.ed_power_1_3(1.0e+09) 
-    p.ed_power_1_4(0.0) 
+    p.ed_power_1_1(0.0)
+    p.ed_power_1_2(1.0e+09)
+    p.ed_power_1_3(1.0e+09)
+    p.ed_power_1_4(0.0)
 
 ### SETUP LASER BEAM ###
-    p.ed_numberOfBeams(1) 
+    p.ed_numberOfBeams(1)
 
 # Setup Gaussian Beam:
-    p.ed_lensX_1(1000.0e-04) 
-    p.ed_lensY_1(0.0e-04) 
-    p.ed_lensZ_1(-1000.0e-04) 
-    p.ed_lensSemiAxisMajor_1(10.0e-04) 
-    p.ed_targetX_1(0.0e-04) 
-    p.ed_targetY_1(0.0e-04) 
-    p.ed_targetZ_1(60.0e-04) 
-    p.ed_targetSemiAxisMajor_1(10.0e-04) 
-    p.ed_targetSemiAxisMinor_1(10.0e-04) 
-    p.ed_pulseNumber_1(1) 
-    p.ed_wavelength_1(1.053) 
-    p.ed_crossSectionFunctionType_1("gaussian2D") 
-    p.ed_gaussianExponent_1(4.0) 
-    p.ed_gaussianRadiusMajor_1(7.5e-04) 
-    p.ed_gaussianRadiusMinor_1(7.5e-04) 
-    p.ed_numberOfRays_1(4096) 
-    p.ed_gridType_1("radial2D") 
-    p.ed_gridnRadialTics_1(64) 
-    p.ed_semiAxisMajorTorsionAngle_1(0.0) 
-    p.ed_semiAxisMajorTorsionAxis_1("x") 
+    p.ed_lensX_1(1000.0e-04)
+    p.ed_lensY_1(0.0e-04)
+    p.ed_lensZ_1(-1000.0e-04)
+    p.ed_lensSemiAxisMajor_1(10.0e-04)
+    p.ed_targetX_1(0.0e-04)
+    p.ed_targetY_1(0.0e-04)
+    p.ed_targetZ_1(60.0e-04)
+    p.ed_targetSemiAxisMajor_1(10.0e-04)
+    p.ed_targetSemiAxisMinor_1(10.0e-04)
+    p.ed_pulseNumber_1(1)
+    p.ed_wavelength_1(1.053)
+    p.ed_crossSectionFunctionType_1("gaussian2D")
+    p.ed_gaussianExponent_1(4.0)
+    p.ed_gaussianRadiusMajor_1(7.5e-04)
+    p.ed_gaussianRadiusMinor_1(7.5e-04)
+    p.ed_numberOfRays_1(4096)
+    p.ed_gridType_1("radial2D")
+    p.ed_gridnRadialTics_1(64)
+    p.ed_semiAxisMajorTorsionAngle_1(0.0)
+    p.ed_semiAxisMajorTorsionAxis_1("x")
 
 
 #################################
@@ -160,19 +287,19 @@ def parms():
 #     CONDUCTION PARAMETERS     #
 #                               #
 #################################
-    p.useDiffuse(True) 
-    p.useConductivity(True) 
-    p.diff_useEleCond(True) 
-    p.diff_eleFlMode("fl_larsen") 
-    p.diff_eleFlCoef(0.06) 
-    p.diff_thetaImplct(1.0) 
+    p.useDiffuse(True)
+    p.useConductivity(True)
+    p.diff_useEleCond(True)
+    p.diff_eleFlMode("fl_larsen")
+    p.diff_eleFlCoef(0.06)
+    p.diff_thetaImplct(1.0)
 
-    p.diff_eleXlBoundaryType("neumann") 
-    p.diff_eleXrBoundaryType("neumann") 
-    p.diff_eleYlBoundaryType("neumann") 
-    p.diff_eleYrBoundaryType("neumann") 
-    p.diff_eleZlBoundaryType("neumann") 
-    p.diff_eleZrBoundaryType("neumann") 
+    p.diff_eleXlBoundaryType("neumann")
+    p.diff_eleXrBoundaryType("neumann")
+    p.diff_eleYlBoundaryType("neumann")
+    p.diff_eleYrBoundaryType("neumann")
+    p.diff_eleZlBoundaryType("neumann")
+    p.diff_eleZrBoundaryType("neumann")
 
 
 ####################################
@@ -180,7 +307,7 @@ def parms():
 #     HEAT EXCHANGE PARAMETERS     #
 #                                  #
 ####################################
-    p.useHeatexchange(True) 
+    p.useHeatexchange(True)
 
 
 ##########################
@@ -188,10 +315,10 @@ def parms():
 #     EOS PARAMETERS     #
 #                        #
 ##########################
-    p.eosModeInit("dens_temp_gather") 
-    p.smallt(1.0) 
-    p.smallx(1.0e-99) 
-    p.eos_useLogTables(False) 
+    p.eosModeInit("dens_temp_gather")
+    p.smallt(1.0)
+    p.smallx(1.0e-99)
+    p.eos_useLogTables(False)
 
 
 ############################
@@ -199,8 +326,8 @@ def parms():
 #     HYDRO PARAMETERS     #
 #                          #
 ############################
-    p.useHydro(True) 
-     
+    p.useHydro(True)
+
     p.order(3) # Interpolation order (first/second/third/fifth order)
     p.slopeLimiter("minmod") # Slope limiters (minmod, mc, vanLeer, hybrid, limited)
     p.LimitedSlopeBeta(1.) # Slope parameter for the "limited" slope by Toro
@@ -216,12 +343,12 @@ def parms():
     p.use_hybridOrder(True) # Enforce Riemann density jump
 
 # Hydro boundary conditions:
-    p.xl_boundary_type("reflect") 
-    p.xr_boundary_type("outflow") 
-    p.yl_boundary_type("outflow") 
-    p.yr_boundary_type("outflow") 
-    p.zl_boundary_type("reflect") 
-    p.zr_boundary_type("reflect") 
+    p.xl_boundary_type("reflect")
+    p.xr_boundary_type("outflow")
+    p.yl_boundary_type("outflow")
+    p.yr_boundary_type("outflow")
+    p.zl_boundary_type("reflect")
+    p.zr_boundary_type("reflect")
 
 
 ##############################
@@ -230,32 +357,46 @@ def parms():
 #                            #
 ##############################
 
-    p.sim_targetRadius(200.0e-04) 
-    p.sim_targetHeight(20.0e-04) 
-    p.sim_vacuumHeight(60.0e-04) 
+    p.sim_targetRadius(200.0e-04)
+    p.sim_vacuumHeight(60.0e-04)
+    p.sim_initGeom("slab")
 
-# Target material defaults set for Aluminum at room temperature:
-    p.sim_rhoTarg(2.7) 
-    p.sim_teleTarg(290.11375) 
-    p.sim_tionTarg(290.11375) 
-    p.sim_tradTarg(290.11375) 
-    p.ms_targA(26.9815386) 
-    p.ms_targZ(13.0) 
-    p.ms_targZMin(0.02) 
-    p.eos_targEosType("eos_tab") 
-    p.eos_targSubType("ionmix4") 
-    p.eos_targTableFile("al-imx-003.cn4") 
+    # Number of target layers
+    p.sim_numLayers(len(LAYERS))
+
+    # Set per-layer parameters
+    for idx, layer in enumerate(LAYERS):
+        n = idx + 1  # 1-indexed for FLASH parameter naming
+        name = layer["name"]
+
+        getattr(p, f"sim_rho_{n}")(layer["rho"])
+        getattr(p, f"sim_tele_{n}")(layer["tele"])
+        getattr(p, f"sim_tion_{n}")(layer["tion"])
+        getattr(p, f"sim_trad_{n}")(layer["trad"])
+        getattr(p, f"sim_height_{n}")(layer["height"])
+        getattr(p, f"sim_radius_{n}")(layer["radius"])
+        getattr(p, f"ms_{name}A")(layer["A"])
+        getattr(p, f"ms_{name}Z")(layer["Z"])
+        if layer.get("zMin", 0.0) > 0:
+            getattr(p, f"ms_{name}ZMin")(layer["zMin"])
+        getattr(p, f"eos_{name}EosType")(layer["eosType"])
+        getattr(p, f"eos_{name}SubType")(layer["eosSubType"])
+        getattr(p, f"eos_{name}TableFile")(layer["eosFile"])
 
 # Chamber material defaults set for Helium at pressure 1.6 mbar:
-    p.sim_rhoCham(1.0e-06) 
-    p.sim_teleCham(290.11375) 
-    p.sim_tionCham(290.11375) 
-    p.sim_tradCham(290.11375) 
-    p.ms_chamA(4.002602) 
-    p.ms_chamZ(2.0) 
-    p.eos_chamEosType("eos_tab") 
-    p.eos_chamSubType("ionmix4") 
-    p.eos_chamTableFile("he-imx-005.cn4") 
+    p.sim_rhoCham(CHAMBER["rho"])
+    p.sim_teleCham(CHAMBER["tele"])
+    p.sim_tionCham(CHAMBER["tion"])
+    p.sim_tradCham(CHAMBER["trad"])
+    p.ms_chamA(CHAMBER["A"])
+    p.ms_chamZ(CHAMBER["Z"])
+    p.eos_chamEosType(CHAMBER["eosType"])
+    p.eos_chamSubType(CHAMBER["eosSubType"])
+    p.eos_chamTableFile(CHAMBER["eosFile"])
+
+    # Custom density map parameters
+    p.sim_customDensMap(CUSTOM_DENS_MAP)
+    p.sim_densMapFile(CUSTOM_DENS_MAP_FILE)
 
 
 ###########################
@@ -263,16 +404,16 @@ def parms():
 #     TIME PARAMETERS     #
 #                         #
 ###########################
-    p.tstep_change_factor(1.10) 
-    p.cfl(0.4) 
+    p.tstep_change_factor(1.10)
+    p.cfl(0.4)
     p.dt_diff_factor(1.0e+100) # Disable diffusion dt
-    p.rt_dtFactor(1.0e+100) 
-    p.hx_dtFactor(1.0e+100) 
-    p.tmax(2.0e-09) 
-    p.dtmin(1.0e-16) 
-    p.dtinit(1.0e-15) 
-    p.dtmax(3.0e-09) 
-    p.nend(10000000) 
+    p.rt_dtFactor(1.0e+100)
+    p.hx_dtFactor(1.0e+100)
+    p.tmax(2.0e-09)
+    p.dtmin(1.0e-16)
+    p.dtinit(1.0e-15)
+    p.dtmax(3.0e-09)
+    p.nend(10000000)
 
 
 ###########################
@@ -280,20 +421,19 @@ def parms():
 #     MESH PARAMETERS     #
 #                         #
 ###########################
-    p.geometry("cylindrical") 
+    p.geometry("cylindrical")
 
 # Domain size:
-    p.xmin(0.0) 
-    p.xmax(40.0e-04) 
-    p.ymin(0.0e-04) 
-    p.ymax(80.0e-04) 
+    p.xmin(0.0)
+    p.xmax(40.0e-04)
+    p.ymin(0.0e-04)
+    p.ymax(80.0e-04)
 
 # Total number of blocks:
-    p.nblockx(1) 
-    p.nblocky(2) 
+    p.nblockx(1)
+    p.nblocky(2)
 
-    p.lrefine_max(4) 
-    p.lrefine_min(1) 
-    p.refine_var_1("dens") 
-    p.refine_var_2("tele") 
-
+    p.lrefine_max(4)
+    p.lrefine_min(1)
+    p.refine_var_1("dens")
+    p.refine_var_2("tele")
--- FoamStructure/pySimulation.py
+++ FoamStructure/pySimulation.py
@@ -7,29 +7,74 @@ from   flash.constantsH import *
 from   flash.Simulation_base import pySimulation
 
 import numpy as np
+import os
 
+# Species index convention:
+#   CHAM_SPEC = 1 (always the first species = chamber/background)
+#   Layer species = 2, 3, 4, ... (in order of LAYERS list)
 if not 'CHAM_SPEC' in locals():
     CHAM_SPEC = 1
-    TARG_SPEC = 2
+
 
 class Simulation(pySimulation):
 
     def init(self):
         eV2K = 11604.5221
+
+        # ----- Geometry parameters -----
         self.targetRadius = p.sim_targetRadius.getVal()
-        self.targetHeight = p.sim_targetHeight.getVal()
         self.vacuumHeight = p.sim_vacuumHeight.getVal()
-        self.rhoTarg      = p.sim_rhoTarg.getVal()
-        self.teleTarg     = p.sim_teleTarg.getVal()
-        self.tionTarg     = p.sim_tionTarg.getVal()
-        self.tradTarg     = p.sim_tradTarg.getVal()
-        self.rhoCham      = p.sim_rhoCham.getVal()
-        self.teleCham     = p.sim_teleCham.getVal()
-        self.tionCham     = p.sim_tionCham.getVal()
-        self.tradCham     = p.sim_tradCham.getVal()
         self.smallX       = p.smallx.getVal()
         self.initGeom     = p.sim_initGeom.getVal()
-        #RadSlab parms
+
+        # ----- Load layer definitions -----
+        self.numLayers = p.sim_numLayers.getVal()
+        self.layers = []
+        for n in range(1, self.numLayers + 1):
+            layer = {
+                "rho":    getattr(p, f"sim_rho_{n}").getVal(),
+                "tele":   getattr(p, f"sim_tele_{n}").getVal(),
+                "tion":   getattr(p, f"sim_tion_{n}").getVal(),
+                "trad":   getattr(p, f"sim_trad_{n}").getVal(),
+                "height": getattr(p, f"sim_height_{n}").getVal(),
+                "radius": getattr(p, f"sim_radius_{n}").getVal(),
+                "spec":   CHAM_SPEC + n,  # species index: 2, 3, 4, ...
+            }
+            self.layers.append(layer)
+
+        # Compute cumulative y-boundaries for sequential stacking
+        # Layer 0: vacuumHeight -> vacuumHeight + height[0]
+        # Layer 1: vacuumHeight + height[0] -> vacuumHeight + height[0] + height[1]
+        # etc.
+        y_start = self.vacuumHeight
+        for layer in self.layers:
+            layer["ymin"] = y_start
+            layer["ymax"] = y_start + layer["height"]
+            y_start = layer["ymax"]
+
+        # ----- Chamber (background) parameters -----
+        self.rhoCham  = p.sim_rhoCham.getVal()
+        self.teleCham = p.sim_teleCham.getVal()
+        self.tionCham = p.sim_tionCham.getVal()
+        self.tradCham = p.sim_tradCham.getVal()
+
+        # ----- Custom density map -----
+        self.customDensMap = p.sim_customDensMap.getVal()
+        if self.customDensMap:
+            densMapFile = p.sim_densMapFile.getVal().strip()
+            print(f"[Simulation] Loading custom density map from: {densMapFile}")
+            data = np.load(densMapFile)
+            self.densMap_x       = data["x"]        # 1D array of x coords
+            self.densMap_y       = data["y"]        # 1D array of y coords
+            self.densMap_density = data["density"]   # 2D array (Nx, Ny)
+            self.densMap_species = data["species"]   # 2D array (Nx, Ny), int
+
+            # Optional: load temperature arrays if present
+            self.densMap_tele = data["tele"] if "tele" in data else None
+            self.densMap_tion = data["tion"] if "tion" in data else None
+            self.densMap_trad = data["trad"] if "trad" in data else None
+
+        # ----- RadSlab parameters -----
         self.radSlab        = p.sim_radSlab.getVal()
         self.radSourceType  = p.sim_radSourceType.getVal()
         self.radSourceTmax  = p.sim_radSourceTMax.getVal()*eV2K
@@ -50,30 +95,94 @@ class Simulation(pySimulation):
         if (self.radSlab and p.useEnergyDeposition.getVal()):
             print ('WARNING: sim_radSlab & useEnergyDeposition both True')
 
+    # ------------------------------------------------------------------
+    #  Geometry helpers: determine which species a coordinate belongs to
+    # ------------------------------------------------------------------
+
     def isTargSlab(self, x, y):
-        if NDIM == 1 :
-            if (x <= self.targetHeight + self.vacuumHeight and 
-                x >= self.vacuumHeight):
-                return TARG_SPEC
+        """Check layers from last (outermost) to first (innermost).
+        For sequential stacking each layer occupies:
+          ymin <= y < ymax  AND  x <= radius
+        Returns the species index, or CHAM_SPEC for the background.
+        """
+        if NDIM == 1:
+            for layer in reversed(self.layers):
+                if layer["ymin"] <= x < layer["ymax"]:
+                    return layer["spec"]
         elif NDIM == 2 or NDIM == 3:
-            if  (x <= self.targetRadius and 
-                 y <= self.targetHeight + self.vacuumHeight and 
-                 y >= self.vacuumHeight):
-                return TARG_SPEC
+            for layer in reversed(self.layers):
+                if (x <= layer["radius"] and
+                    layer["ymin"] <= y < layer["ymax"]):
+                    return layer["spec"]
 
         return CHAM_SPEC
 
     def isTargSphere(self, x, y, z):
+        """Sphere geometry: only uses first layer's radius."""
         r = np.sqrt(x*x + y*y + z*z)
-        if r <= self.targetRadius:
-            return TARG_SPEC
+        if self.numLayers > 0 and r <= self.layers[0]["radius"]:
+            return self.layers[0]["spec"]
         return CHAM_SPEC
 
     def isTarg(self, x, y, z):
         if self.initGeom == "slab":
-            return self.isTargSlab(x,y)
+            return self.isTargSlab(x, y)
         else:
             return self.isTargSphere(x, y, z)
+
+    # ------------------------------------------------------------------
+    #  Custom density map lookup
+    # ------------------------------------------------------------------
+
+    def _lookup_densmap(self, x, y):
+        """Look up density and species from the loaded .npz arrays
+        using nearest-neighbour interpolation on the 1D coord arrays.
+        Returns (rho, species_index, tele, tion, trad).
+        """
+        ix = np.searchsorted(self.densMap_x, x, side='right') - 1
+        iy = np.searchsorted(self.densMap_y, y, side='right') - 1
+
+        # Clamp to valid range
+        ix = max(0, min(ix, len(self.densMap_x) - 1))
+        iy = max(0, min(iy, len(self.densMap_y) - 1))
+
+        rho     = self.densMap_density[ix, iy]
+        spec_id = int(self.densMap_species[ix, iy])
+
+        # Map species_id -> material properties for temperature
+        # spec_id: 0 = cham, 1 = layer 1, 2 = layer 2, ...
+        if spec_id == 0:
+            tele = self.teleCham
+            tion = self.tionCham
+            trad = self.tradCham
+        elif 1 <= spec_id <= self.numLayers:
+            layer = self.layers[spec_id - 1]
+            tele = layer["tele"]
+            tion = layer["tion"]
+            trad = layer["trad"]
+        else:
+            # Fallback to chamber
+            tele = self.teleCham
+            tion = self.tionCham
+            trad = self.tradCham
+
+        # Override with per-cell temperatures from map if available
+        if self.densMap_tele is not None:
+            tele = self.densMap_tele[ix, iy]
+        if self.densMap_tion is not None:
+            tion = self.densMap_tion[ix, iy]
+        if self.densMap_trad is not None:
+            trad = self.densMap_trad[ix, iy]
+
+        # Convert spec_id (0-based) to FLASH species index (1-based)
+        # 0 -> CHAM_SPEC (1), 1 -> CHAM_SPEC+1 (2), etc.
+        species = CHAM_SPEC + spec_id
+
+        return rho, species, tele, tion, trad
+
+    # ------------------------------------------------------------------
+    #  Block initialisation
+    # ------------------------------------------------------------------
 
     def initBlock(self, blockID):
         block = grid.Block(blockID)
@@ -83,17 +192,31 @@ class Simulation(pySimulation):
         zz = np.array(block.zCoord)
 
         for (i,j,k) in np.ndindex(solnVec.shape[1:]):
-            rho  = self.rhoCham
-            tele = self.teleCham
-            tion = self.tionCham
-            trad = self.tradCham
 
-            species = self.isTarg(xx[i], yy[j], zz[k])
-            if species == TARG_SPEC:
-                rho  = self.rhoTarg
-                tele = self.teleTarg
-                tion = self.tionTarg
-                trad = self.tradTarg
+            if self.customDensMap:
+                # --- Custom density map path ---
+                rho, species, tele, tion, trad = self._lookup_densmap(
+                    xx[i], yy[j])
+            else:
+                # --- Geometric layer path ---
+                # Default to chamber values
+                rho  = self.rhoCham
+                tele = self.teleCham
+                tion = self.tionCham
+                trad = self.tradCham
+                species = CHAM_SPEC
+
+                spec_id = self.isTarg(xx[i], yy[j], zz[k])
+                if spec_id != CHAM_SPEC:
+                    # Find the matching layer (spec_id = CHAM_SPEC + layer_index + 1)
+                    layer_idx = spec_id - CHAM_SPEC - 1
+                    if 0 <= layer_idx < self.numLayers:
+                        layer = self.layers[layer_idx]
+                        rho  = layer["rho"]
+                        tele = layer["tele"]
+                        tion = layer["tion"]
+                        trad = layer["trad"]
+                    species = spec_id
 
             solnVec[DENS_VAR,i,j,k] = rho
             solnVec[TEMP_VAR,i,j,k] = tele
@@ -151,7 +274,3 @@ class Simulation(pySimulation):
         for f in range(2*NDIM):
             if self.mgdBC[f] == 'DIRICHLET':
                 [rt.mgdSetBC(ig=n, f=f, bcType=DIRICHLET, bcValue=urad[n]) for n in range(self.nG)]
-
-
-
-
