288d649e5d2dec082eb2c2cd68933f8f0e55afe4
Daniel <92740330+PhysicsDan@users.noreply.github.com>
2026-04-23 11:41:04 +0100

    Update autocompile and config

--- daniel-mac-autocompile.sh
+++ daniel-mac-autocompile.sh
@@ -2,12 +2,19 @@
 FLASH_DIR="/Users/dmolloy/Documents/Simulations/FLASH4.8"
 SIM_DIR="/Users/dmolloy/Documents/Simulations/flash/flash_test"
 
+echo "Activating python env"
 source "$(brew --prefix)/Caskroom/miniconda/base/etc/profile.d/conda.sh"
 conda activate flash
 
+echo "Generating config file"
+cd "$FLASH_DIR/source/Simulation/SimulationMain/python/LaserSlabFoam/FoamStructure"
+python generateConfig.py
+
+echo "Running setup"
 cd "$FLASH_DIR"
 ./setup python/LaserSlabFoam/FoamStructure --site=daniel.macos
 
+echo "Compiling executable"
 cd "$SIM_DIR"
 rm -rf *
 cmake "$FLASH_DIR/object"
